d07083cdf45e409adb918b296987157e4ed453f1
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-02-08 08:43:45 +0100

    allow to push links directly

--- bg.js
+++ bg.js
@@ -2,9 +2,20 @@
 
 chrome.contextMenus.create({
 	title: 'read later',
+	contexts: ['page'],
 	onclick: function(info, tab) {
 		pushBookmark(tab, function() {
 			chrome.tabs.remove(tab.id);
+		});
+	},
+});
+
+chrome.contextMenus.create({
+	title: 'read link later',
+	contexts: ['link'],
+	onclick: function(info) {
+		pushBookmark({
+			url: info.linkUrl,
 		});
 	},
 });
