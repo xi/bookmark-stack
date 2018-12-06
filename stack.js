7aa5f1eb3c07ce79158f9ff45ed879d759311d24
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-12-06 11:37:17 +0100

    show message on empty stack

--- stack.js
+++ stack.js
@@ -1,31 +1,35 @@
 'use struct';
 
 var render = function(element, items) {
-	element.innerHTML = '';
-	items.forEach(function(item) {
-		var icon = document.createElement('img');
-		icon.className = 'icon';
+	if (items.length === 0) {
+		element.outerHTML = '<p>No links stored yet.</p>'
+	} else {
+		element.innerHTML = '';
+		items.forEach(function(item) {
+			var icon = document.createElement('img');
+			icon.className = 'icon';
 
-		if (chrome && typeof browser === 'undefined') {
-			icon.src = 'chrome://favicon/' + item.url;
-		} else {
-			var rootUrl = item.url.split('/').slice(0, 3).join('/');
-			icon.src = rootUrl + '/favicon.ico';
-		}
+			if (chrome && typeof browser === 'undefined') {
+				icon.src = 'chrome://favicon/' + item.url;
+			} else {
+				var rootUrl = item.url.split('/').slice(0, 3).join('/');
+				icon.src = rootUrl + '/favicon.ico';
+			}
 
-		var a = document.createElement('a');
-		a.id = item.id;
-		a.href = item.url;
-		a.title = item.url;
-		a.textContent = item.title || item.url;
-		a.prepend(icon);
+			var a = document.createElement('a');
+			a.id = item.id;
+			a.href = item.url;
+			a.title = item.url;
+			a.textContent = item.title || item.url;
+			a.prepend(icon);
 
-		var li = document.createElement('li');
-		li.appendChild(a);
+			var li = document.createElement('li');
+			li.appendChild(a);
 
-		// reverse
-		element.prepend(li);
-	});
+			// reverse
+			element.prepend(li);
+		});
+	}
 };
 
 getBookmarks(function(bookmarks) {
